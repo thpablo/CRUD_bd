9235b6a9f78aa6ae52ca705dffff4fb7d8916632
Pablo Almeida <pabloalmeida49@hotmail.com>
2025-07-31 20:32:57 -0300

    change user and password auth of database

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user:password@localhost/db_name'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://myuser:password@localhost/mydatabase'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
